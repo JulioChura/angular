38244485406c76d6251259a62e41039b00b7e8a9
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-01 11:14:07 -0500

    Se edita los estilos , el html y el ts de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Mi Primer App en Angular';
+  title = 'mi-dream-app';
+  name = "Julio Chura";
+  email = "jchuraaca@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
 }
